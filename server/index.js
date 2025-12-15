2c6191360e090d951966c2b39ecc7f243e46d24f
Prj-geek <2727priyanshu@gmail.com>
2025-12-15 22:46:18 +0530

    Add stats API to expose click analytics and connected database using MongoDB

--- server/index.js
+++ server/index.js
@@ -1,20 +1,44 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 app.use(express.json());
 
-// Temporary in-memory storage (no database yet)
-const urls = {};  
-// Example: urls["abc123"] = "https://google.com"
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// URL schema (structure of data)
+const UrlSchema = new mongoose.Schema({
+  shortId: String,
+  originalUrl: String,
+  clicks: {
+    type: Number,
+    default: 0
+  },
+    createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create model
+const Url = mongoose.model('Url', UrlSchema);
 
 // Create short URL
-app.post('/shorten', (req, res) => {
+app.post('/shorten', async (req, res) => {
   const originalUrl = req.body.url;
 
-  // Create a simple random ID
   const id = Math.random().toString(36).substring(2, 8);
 
-  urls[id] = originalUrl;
+  const newUrl = new Url({
+    shortId: id,
+    originalUrl: originalUrl
+  });
+
+  await newUrl.save();
 
   res.json({
     shortId: id,
@@ -22,17 +46,39 @@ app.post('/shorten', (req, res) => {
   });
 });
 
-// Redirect
-app.get('/:id', (req, res) => {
+app.get('/stats/:id', async (req, res) => {
   const id = req.params.id;
-  const originalUrl = urls[id];
 
-  if (!originalUrl) return res.status(404).send("Not found");
+  const urlData = await Url.findOne({ shortId: id });
 
-  res.redirect(originalUrl);
+  if (!urlData) {
+    return res.status(404).json({ error: "Short URL not found" });
+  }
+
+  res.json({
+    shortId: urlData.shortId,
+    originalUrl: urlData.originalUrl,
+    clicks: urlData.clicks,
+    createdAt: urlData.createdAt
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+
+// Redirect
+app.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const urlData = await Url.findOne({ shortId: id });
+
+  if (!urlData) return res.status(404).send("Not found");
+
+  // increment click count
+  urlData.clicks += 1;
+  await urlData.save();
+
+  res.redirect(urlData.originalUrl);
 });
 
+app.listen(process.env.PORT, () => {
+  console.log("Server running");
+});
